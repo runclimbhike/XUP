593d17ec275d1de60ca7d8bbabed709c94b361d2
runclimbhike <runclimbhike@users.noreply.github.com>
2013-09-30 08:31:51 -0500

    Update XUP.sh

--- XUP.sh
+++ XUP.sh
@@ -2,9 +2,9 @@
 # Name: XUP
 # Author: runclimbhike
 # Date: 9/11/13
-# Program Description: This program controls access to an encrypted file using EncFS for encryption.
-# Details: This program assumes that the EncFS file has been created already. 
-#
+# Program Description: This is a control program for access to an encrypted file using EncFS for encryption.
+# Details: 
+# Dependencies: EncFS, a lightweight text editor
 #
 #begin splash and program information.
 echo '
@@ -19,64 +19,114 @@ echo '
 '
 echo 'Initializing'
 sleep 1
-echo 'Fully licensed under General Public License 3. *** Copyleft only *** You cant hate it if you made it***'
-sleep 1
-echo 'Please enter your credentials.'
-sleep 1
-#begin actual program
-#use enfs to mount the encrypted folder to decrypted
-encfs ~/.encrypted ~/.decrypted
-#user will enter the password
-if [ -a ~/.decrypted/data ];
-then
-	echo 'Verifying...'
-else 
-	echo 'Your credentials could not be verified. XUP will now close.'
-	sleep 4
-	clear
+echo 'Fully licensed under General Public License 3 *** Copyleft only *** You cant hate it if you made it***'
 
-	exit 0
-fi
-sleep 1
-echo 'Your credentials have been verified successfully.'
-sleep 1
-echo 'XUP will now open a 5 minute session after which the file will re-encrypt.'
-sleep 1
-#spawn another terminal that opens the file in EDITOR
 
-kwrite ~/.decrypted/data & 
+until [ "$selection" = "0"]; do
+     echo ""
+     echo "XUP PROGRAM MENU"
+     echo "1 - Create encFS stash"
+     echo "2 - Remove encFS stash"
+     echo "3 - Open stash for 6 minutes"
+     echo "4 - Open stash indefinitely"
+     echo "5 - Open stash read-only"
+     echo "0 - Exit program"
+     echo ""
+     echo -n "Enter Selection:"
+     read selection
+     echo ""
+     case $selection in
+         1 ) ls -f;;
+         2 ) echo "This removes files"
+             echo "Please type name of file to remove"
+             read deletefiles  
+             rm $deletefiles;;
+         3 ) echo 'Please enter your credentials.'
+		sleep 1
+		#begin actual program
+		#use enfs to mount the encrypted folder to decrypted
+		encfs ~/.encrypted ~/.decrypted
+		#user will enter the password
+		if [ -a ~/.decrypted/data ];
+		then
+			echo 'Verifying...'
+			echo '.'
+		else 
+			echo 'Your credentials could not be verified. XUP will now close.'
+		sleep 4
+		clear
 
-# Allow 6 minutes of access. gedit is set to save every minute.
-# Set gedit to save every minute.
+		exit 0
+		fi
+		sleep 1
+		echo '.'
+		sleep 1
+		echo 'Your credentials have been verified successfully.'
+		sleep 1
+		echo 'XUP will now open a 5 minute session after which the file will re-encrypt.'
+		sleep 1
+		#spawn another terminal that opens the file in gedit
 
-sleep 360
+		kwrite ~/.decrypted/data & 
 
-#close the spawned gedit process after the 2 minute save has completed.
+		# Allow 2 minutes of access. gedit is set to save every minute.
+		# Set EDITOR to save every minute.
 
-pkill kwrite
+		sleep 420
 
-#encrypt the folder again
+		#close the spawned gedit process after the 2 minute save has completed.
 
-fusermount -u ~/.decrypted
-#verify that the data is no long decrypted
-if [ -a ~/.decrypted/data ];
-then 
-	clear
-	echo 'Folder has not been encrypted correctly. Please see admin.'
-else	
-	clear
-	echo '
-*****************************************************
-Session complete. 
+		pkill kwrite
 
-There are no records of any activity performed on 
-the file during this time period.
- 
-Your secure file has been re-encrypted successfully.
-*****************************************************'
-	sleep 5
-	clear
-fi
+		#encrypt the folder again
+
+		fusermount -u ~/.decrypted
+		#verify that the data is no long decrypted
+		if [ -a ~/.decrypted/data ];
+			then 
+		clear
+			echo 'Folder has not been encrypted correctly. Please see admin.'
+		else	
+		clear
+		echo '
+		*****************************************************
+		Session complete. 
+
+		Your secure file has been re-encrypted successfully.
+		*****************************************************'
+		sleep 5
+		clear
+		fi;;
+
+         4 ) echo "This makes a Directory"
+             echo "Please enter Directory name"
+             read destination
+             mkdir $destination;;
+         0 ) exit;;
+         
+         * ) echo "Please enter 1,2,3,4 or 0"
+         
+     esac
+     
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

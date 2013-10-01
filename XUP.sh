4a4988795a0465b67399e74085f5a0fdd67dc58b
runclimbhike <runclimbhike@users.noreply.github.com>
2013-10-01 15:42:34 -0500

    Update XUP.sh

--- XUP.sh
+++ XUP.sh
@@ -4,7 +4,7 @@
 # Date: 9/11/13
 # Program Description: This is a control program for access to an encrypted file using EncFS for encryption.
 # Details: 
-# Dependencies: EncFS, a lightweight text editor
+# Dependencies: EncFS, fuse and an editor
 #
 #begin splash and program information.
 echo '
@@ -19,7 +19,7 @@ echo '
 '
 echo 'Initializing'
 sleep 1
-echo 'Fully licensed under General Public License 3 *** Copyleft only *** You cant hate it if you made it***'
+echo 'Fully licensed under General Public License 3 *** Copyleft only ***'
 
 selection=
 until [ "$selection" = "0"]; do
@@ -29,19 +29,110 @@ until [ "$selection" = "0"]; do
      echo "2 - Remove encFS stash"
      echo "3 - Open stash for 6 minutes"
      echo "4 - Open stash indefinitely"
-     echo "5 - Open stash read-only"
      echo "0 - Exit program"
      echo ""
      echo -n "Enter Selection:"
      read selection
      echo ""
      case $selection in
-         1 ) ls -f;;
-         2 ) echo "This removes files"
-             echo "Please type name of file to remove"
-             read deletefiles  
-             rm $deletefiles;;
-         3 ) echo 'Please enter your credentials.'
+# Begin creation of encfs stash
+         1 ) 
+# check if encfs and fusermount is installed
+			echo "Checking if software dependencies are installed"
+			sleep 1
+			 command -v encfs >/dev/null 2>&1 || { echo >&2 "XUP requires fuse but it's not installed.  Aborting."; exit 1; }
+			 command -v fusermount >/dev/null 2>&1 || { echo >&2 "XUP requires encfs but it's not installed.  Aborting."; exit 1; }
+# Setup a basic text editor to open the encrypted file
+			
+			if [ -a /usr/bin/kwrite ];
+				then echo "Software dependencies have been satisfied. Beginning install."
+					 EDITOR=kwrite
+				elif [ -a /usr/bin/gedit ];
+				then echo "Software dependencies have been satisfied. Beginning install."
+					 EDITOR=gedit
+				else echo "Please install either kwrite or gedit."
+					exit 0
+				
+			fi
+			
+					 
+# create password, choose initial settings and initiate mount points.
+			echo "Software dependencies have been satisfied. Beginning install."
+					encfs ~/.encrypted ~/.decrypted
+				
+		;;
+# Remove stash
+
+		 2 ) 
+			rm -rf ~/.decrypted
+			rm -rf ~/.encrypted
+			echo Your encrypted stash has been deleted.
+			;;
+# Open stash for 6 minutes.
+
+		 3 ) 
+         
+			echo "Please enter your credentials."
+			sleep 1
+			#begin actual program
+			#use enfs to mount the encrypted folder to decrypted
+			encfs ~/.encrypted ~/.decrypted
+			#user will enter the password
+			if [ -a ~/.decrypted/data ];
+			then
+				echo 'Verifying...'
+				
+			else 
+				echo 'Your credentials could not be verified. XUP will now close.'
+			sleep 4
+			clear
+
+			exit 0
+			fi
+			sleep 1
+			echo '.'
+			sleep 1
+			echo 'Your credentials have been verified successfully.'
+			sleep 1
+			echo 'XUP will now open a 5 minute session after which the file will re-encrypt.'
+			sleep 1
+			#spawn another terminal that opens the file in gedit
+
+			EDITOR ~/.decrypted/data & 
+
+			# Allow 2 minutes of access. gedit is set to save every minute.
+			# Set EDITOR to save every minute.
+
+			sleep 420
+
+			#close the spawned gedit process after the 2 minute save has completed.
+
+			pkill EDITOR
+
+			#encrypt the folder again
+
+			fusermount -u ~/.decrypted
+			#verify that the data is no long decrypted
+			if [ -a ~/.decrypted/data ];
+				then 
+			clear
+				echo 'Folder has not been encrypted correctly. Please see admin.'
+			else	
+			clear
+			echo '
+			*****************************************************
+			Session complete. 
+	
+			Your secure file has been re-encrypted successfully.
+			*****************************************************'
+			sleep 5
+			clear
+			fi;;
+
+# Open stash indefinitely
+
+         4 ) 
+         echo 'Please enter your credentials.'
 		sleep 1
 		#begin actual program
 		#use enfs to mount the encrypted folder to decrypted
@@ -63,45 +154,19 @@ until [ "$selection" = "0"]; do
 		sleep 1
 		echo 'Your credentials have been verified successfully.'
 		sleep 1
-		echo 'XUP will now open a 5 minute session after which the file will re-encrypt.'
+		echo 'XUP will now open a session and close.'
 		sleep 1
 		#spawn another terminal that opens the file in gedit
 
-		kwrite ~/.decrypted/data & 
+		EDITOR ~/.decrypted/data & 
 
-		# Allow 2 minutes of access. gedit is set to save every minute.
-		# Set EDITOR to save every minute.
+             
+# Close the parent shell and leave the EDITOR open indefinitly.
 
-		sleep 420
-
-		#close the spawned gedit process after the 2 minute save has completed.
-
-		pkill kwrite
-
-		#encrypt the folder again
-
-		fusermount -u ~/.decrypted
-		#verify that the data is no long decrypted
-		if [ -a ~/.decrypted/data ];
-			then 
-		clear
-			echo 'Folder has not been encrypted correctly. Please see admin.'
-		else	
-		clear
-		echo '
-		*****************************************************
-		Session complete. 
-
-		Your secure file has been re-encrypted successfully.
-		*****************************************************'
-		sleep 5
-		clear
-		fi;;
-
-         4 ) echo "This makes a Directory"
-             echo "Please enter Directory name"
-             read destination
-             mkdir $destination;;
+		exit;;
+		
+		
+		
          0 ) exit;;
          
          * ) echo "Please enter 1,2,3,4 or 0"
